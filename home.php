7b5d57e0837b4f888bbdab7645b303a5f3225528
nolivo1 <nolivo1@gmail.com>
2014-12-10 21:54:10 -0500

    fixin shit

--- home.php
+++ home.php
@@ -1,10 +1,10 @@
 <section class="home body"><!-- Home Template touch -->	
 	<ul class="bxslider">
-		<li><img src="imgs/suite-01a.jpg" /></li>
-		<li><img src="imgs/suite-02a.png" /></li>
-		<li><img src="imgs/suite-03a.png" /></li>
-		<li><img src="imgs/suite-04a.jpg" /></li>
-		<li><img src="imgs/suite-05a.png" /></li>		
+		<li><img src="imgs/suite-1.jpg" /></li>
+		<li><img src="imgs/suite-2.png" /></li>
+		<li><img src="imgs/suite-3.png" /></li>
+		<li><img src="imgs/suite-4.jpg" /></li>
+		<li><img src="imgs/suite-5.png" /></li>		
 	</ul>
 	<p class="number">Call (917) 412-9722</p>
 	<script type="text/javascript" src="js/home.js"></script>

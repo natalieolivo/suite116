0e5da174167a8625e38ec733eeef10bf3d2c951f
nolivo1 <nolivo1@gmail.com>
2014-12-10 22:29:12 -0500

    upload new imgeees

--- home.php
+++ home.php
@@ -1,10 +1,11 @@
 <section class="home body"><!-- Home Template touch -->	
 	<ul class="bxslider">
-		<li><img src="imgs/suite-1.jpg" /></li>
-		<li><img src="imgs/suite-2.png" /></li>
-		<li><img src="imgs/suite-3.png" /></li>
-		<li><img src="imgs/suite-4.jpg" /></li>
-		<li><img src="imgs/suite-5.png" /></li>		
+		<li><img src="imgs/3.JPG" /></li>
+		<li><img src="imgs/2.JPG" /></li>
+		<li><img src="imgs/4.JPG" /></li>
+		<li><img src="imgs/5.JPG" /></li>
+		<li><img src="imgs/8.JPG" /></li>
+		<li><img src="imgs/9.JPG" /></li>		
 	</ul>
 	<p class="number">Call (917) 412-9722</p>
 	<script type="text/javascript" src="js/home.js"></script>

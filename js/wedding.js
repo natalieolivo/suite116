7581a6dd8d8e059d65c828b43207019976016fa8
nolivo1 <nolivo1@gmail.com>
2014-12-12 11:36:04 -0500

    fix wedding slider

--- js/wedding.js
+++ js/wedding.js
@@ -21,6 +21,32 @@ $(document).ready(function(){
           $(".page").append(template(data));
           S116.Form.setValidationFields();
           S116.Form.checkForErrors();
+          S116.Page = (function($pageSlider){
+            $(".burger").on("click", function() {
+                if($(this).siblings('.navlist').hasClass('expand')) {
+                    $(this).siblings('.navlist').removeClass('expand');
+                } else {
+                    $(this).siblings('.navlist').addClass('expand');
+                }
+            });   
+            if($pageSlider.length > 0) {        
+                $pageSlider.bxSlider({
+                    auto: true,
+                    autoControls: true,
+                    mode: 'fade'
+                });
+                return this;    
+            }
+            $(document).on("pageReady", function(event, slider){               
+                if(slider.length<1) return;       
+                $(slider).bxSlider({
+                    auto: true,
+                    autoControls: true,
+                    mode: 'fade'
+                });
+                return this;
+            });
+          })($(".bxslider"));
           $(document).trigger("pageReady", [".bxslider"]);             
         }
       });    
